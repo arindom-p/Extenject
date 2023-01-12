2860ac385e38b6ff1360340ff39e73d758b9a7d9
Arindom Das <arindamdas.ad214@gmail.com>
2023-01-13 02:32:35 +0600

    last commit in version:2019.3

---
+++ UnityProject/Assets/Scripts/Controller/MotionController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class MotionController : MonoBehaviour
+{
+    [SerializeField] private RectTransform[] roadRTs;
+
+    private readonly int resetDistance = 10000;
+    private int currentDistance,
+        currentSpeed;
+    private bool isMoving = false;
+
+    void Start()
+    {
+        if (roadRTs.Length != 2) Debug.LogError("number of referenced road instances must be 2");
+    }
+
+    private void Update()
+    {
+
+    }
+
+    public void StartingDisplay()
+    {
+
+    }
+
+
+}
