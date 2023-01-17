a79cd70685fab09868cdeb2eaf70e48a79b1d960
Arindom Das <arindamdas.ad214@gmail.com>
2023-01-18 00:42:00 +0600

    infinite car display ability added

--- UnityProject/Assets/Scripts/OtherControllers/GameController.Extended.cs
+++ UnityProject/Assets/Scripts/OtherControllers/GameController.Extended.cs
@@ -6,36 +6,36 @@ public partial class GameController
 {
     private void ShowInitialCarSelectionView()
     {
+        carImages[1].color = Color.clear;
         displayCarIndex = 1;
-        carImages[0].sprite = cars[0].sprite;
-        carImages[1].sprite = cars[1].sprite;
         AnimateCarView(false);
     }
 
     private void AnimateCarView(bool showNext)
     {
+        float centeredImageTargetPosX = 100;
         if (showNext) displayCarIndex++;
         else displayCarIndex--;
-
+        int showImageIndex = this.displayCarIndex % 2;
+        int hideImageIndex = (showImageIndex + 1) % 2;
         carPrevButton.interactable = displayCarIndex > 0;
-        carNextButton.interactable = displayCarIndex < 1;
-        Transform t1 = carImages[0].transform;
-        Transform t2 = carImages[1].transform;
-        Image img1 = carImages[0];
-        Image img2 = carImages[1];
-        float imgTargetPosX = 100;
-        if (displayCarIndex == 1)
+        carNextButton.interactable = displayCarIndex < cars.Length-1;
+        if (showNext)
         {
-            (img1, img2) = (img2, img1);
-            (t1, t2) = (t2, t1);
-            imgTargetPosX = -imgTargetPosX;
+            centeredImageTargetPosX = -centeredImageTargetPosX;
         }
-        t1.DOLocalMoveX(0, Helper.CarImageTransitionDuration);
-        t2.DOLocalMoveX(imgTargetPosX, Helper.CarImageTransitionDuration);
-        img1.color = Color.clear;
-        img2.color = Color.white;
-        img1.DOColor(Color.white, Helper.CarImageTransitionDuration);
-        img2.DOColor(Color.clear, Helper.CarImageTransitionDuration);
+        carImages[showImageIndex].sprite = cars[displayCarIndex].sprite;
+        carImages[showImageIndex].color = Color.clear;
+        carImages[hideImageIndex].color = Color.white;
+        carImages[showImageIndex].DOColor(Color.white, Helper.CarImageTransitionDuration);
+        carImages[hideImageIndex].DOColor(Color.clear, Helper.CarImageTransitionDuration);
+        RectTransform rtShow = carImages[showImageIndex].GetComponent<RectTransform>();
+        RectTransform rtHide = carImages[hideImageIndex].GetComponent<RectTransform>();
+        rtShow.anchoredPosition = centeredImageTargetPosX * Vector3.left;
+        rtHide.anchoredPosition = Vector3.zero;
+        DOTween.Kill(rtShow);
+        rtShow.DOAnchorPos(Vector3.zero, Helper.CarImageTransitionDuration);
+        rtHide.DOAnchorPos(centeredImageTargetPosX * Vector3.right, Helper.CarImageTransitionDuration);
 
         float halfTime = Helper.CarImageTransitionDuration / 2;
         carSpeedBarImage.DOFillAmount(0, halfTime);
